c7316ca089865f80648ce49c847f1b35c3db2328
collins-orian <inaboyacollins@icloud.com>
2023-07-16 01:51:32 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -9,6 +9,6 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    );
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+);
